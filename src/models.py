55eb84506d753486969513ba59ee25997dbc98dc
zdian <zdian726@gmail.com>
2024-10-29 15:44:42 +0800

    Update models.py

--- src/models.py
+++ src/models.py
@@ -14,11 +14,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     input_base_url: str = "https://apiff14risingstones.web.sdo.com"
-    input_cookie: InputCookie = Field(default=...)
-    input_user_agent: str = (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,"
-        " like Gecko) Chrome/129.0.0.0 Safari/537.36"
-    )
+    input_cookie: str = "..."
+    input_user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
     input_comment_content: str = '<p><span class="at-emo">[emo6]</span>&nbsp;</p>'
     input_like_post_id: int = 8
     input_comment_post_id: int = 8
